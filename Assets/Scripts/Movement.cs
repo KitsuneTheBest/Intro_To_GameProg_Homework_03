006a3edccaa2491d0f7026bc18e3db32b51d70cc
KitsuneTheBest <baxa.blackburn@gmail.com>
2022-03-07 19:30:58 +0300

    Added some mechanics
    Added:
    movement mechanics
    rotation mechanics
    
    Created:
    Capsule with face that moves according to movement direction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float Speed = 3f;
+    public float AngularSpeed = 360f;
     void Update()
     {
+        // movement direction
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += movement * Speed * Time.deltaTime;
         
+        if (movement == Vector3.zero)
+        {
+            return;
+        }
+        
+        // rotation relative to the movement direction
+        var rotation = Quaternion.LookRotation(movement);
+        rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            rotation,
+            AngularSpeed * Time.deltaTime);
+        transform.rotation = rotation;
     }
+
 }
